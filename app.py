0c63f7cf0a82f55f179d231a201c21ebdcf81453
mzy <1760103253@qq.com>
2026-02-16 08:40:07 +0800

    修复bug

--- app.py
+++ app.py
@@ -950,5 +950,5 @@ if __name__ == '__main__':
     print("按 Ctrl+C 停止服务器")
     print("=" * 60 + "\n")
     
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000, use_reloader=False)
 
